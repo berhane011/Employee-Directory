1cf63120b08049be126854ac7e2dc04043f93277
berhane <berhane4321@gmail.com>
2020-08-29 14:45:50 -0700

    Updates

---
+++ precache-manifest.98307de3916dc1b485f68e01201bbf0d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da88151662b9853c14e4dca87a236377",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "cebe582131554313ee91",
+    "url": "/Employee-Directory/static/js/2.8eb85c42.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Employee-Directory/static/js/2.8eb85c42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b72bbf0f25757e689f4",
+    "url": "/Employee-Directory/static/js/main.6edb4763.chunk.js"
+  },
+  {
+    "revision": "21ab4dbde2f8e2bfdf72",
+    "url": "/Employee-Directory/static/js/runtime-main.965eecad.js"
+  },
+  {
+    "revision": "d297cb90ed368807af69c107a008a52a",
+    "url": "/Employee-Directory/static/media/p1.d297cb90.jpg"
+  },
+  {
+    "revision": "ade92f6ed60a922230a851f5c3f83a77",
+    "url": "/Employee-Directory/static/media/p4.ade92f6e.jpg"
+  },
+  {
+    "revision": "912a7b71c073e795f0fd205506770127",
+    "url": "/Employee-Directory/static/media/p5.912a7b71.jpg"
+  },
+  {
+    "revision": "38c6d378074203d19a8bf61a222fd026",
+    "url": "/Employee-Directory/static/media/p6.38c6d378.jpg"
+  },
+  {
+    "revision": "306d1e5943fd08314b3b35afe1b8cad4",
+    "url": "/Employee-Directory/static/media/p7.306d1e59.jpg"
+  }
+]);
